badc281999c47e73b02b6cf6e89bc5901669b1f8
Scott <scottboragay06@gmail.com>
2025-07-07 17:28:34 +0800

    added: color, vertex shapes, keys control

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,67 @@
 import org.lwjgl.Version;
 import org.lwjgl.glfw.GLFWVidMode;
 import org.lwjgl.opengl.GL;
+import static org.lwjgl.opengl.GL11.*;
+
+import static org.lwjgl.glfw.GLFW.*;
 
 import static org.lwjgl.glfw.GLFW.*;
 public class Main {
     public static void main(String[] args) {
         System.out.println("LWJGL Version: " + Version.getVersion());
-        if(!glfwInit()){
-            throw new IllegalStateException("failed to start glfw");
-        }
-        glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
-        long window = glfwCreateWindow(640, 400, "glfw game", 0, 0);
-        if(window == 0){
-            throw new IllegalStateException("failed to create window");
-        }
-        glfwMakeContextCurrent(window);
+            if(!glfwInit()){
+                throw new IllegalStateException("failed to load glfw");
+            }
+            glfwWindowHint(GLFW_VISIBLE,GLFW_FALSE);
+            long window = glfwCreateWindow(640, 400, "my glfw game", 0, 0);
 
-        GL.createCapabilities();
+            GLFWVidMode vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
 
-        GLFWVidMode vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+            glfwSetWindowPos(window, (vidmode.width() - (640)) / 2, (vidmode.height() - (400)) / 2) ;
+            glfwShowWindow(window);
 
-        glfwSetWindowPos(window, (vidmode.width() - 640) / 2, ( vidmode.height() - 400) / 2);
+            //PASS CONTEXT
+            glfwMakeContextCurrent(window);
+            GL.createCapabilities();
 
-        glfwShowWindow(window);
 
-        while(!glfwWindowShouldClose(window)){
-            glfwPollEvents();
-            glfwSwapBuffers(window);
-        }
+            //clear color to
+            glClearColor(0f, 0f, 0f, 1f);
+
+            float x = 0;
+            float y = 0;
+
+            while(!glfwWindowShouldClose(window)){
+                glClear(GL_COLOR_BUFFER_BIT);
+                glfwPollEvents();
+
+                if(glfwGetKey(window, GLFW_KEY_RIGHT) == 1){
+                    x += 0.001f;
+                }
+                if(glfwGetKey(window, GLFW_KEY_UP) == 1){
+                    y += 0.001f;
+                }
+                if(glfwGetKey(window, GLFW_KEY_LEFT) == 1){
+                    x -= 0.001f;
+                }
+                if(glfwGetKey(window, GLFW_KEY_DOWN) == 1){
+                    y -= 0.001f;
+                }
+                glBegin(GL_QUADS);
+                glColor4f(0f, 1f, 0f, 1f);
+                glVertex2f(-0.5f + x, 0.5f + y);
+                glColor4f(1f,0f,0f,1f);
+                glVertex2f(-0.5f + x, -0.5f + y);
+                glColor4f(0f,0f,1f,1f);
+                glVertex2f(0.5f + x, -0.5f + y);
+                glColor4f(0f, 0f, 0f, 1f);
+                glVertex2f(0.5f + x, 0.5f + y);
+                glEnd();
+
+                glfwSwapBuffers(window);
+            }
             glfwTerminate();
+
 
         }
 }
