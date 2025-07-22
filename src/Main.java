6ea425c54df86ebaf838753db4305e40ccaf9458
Scott <scottboragay06@gmail.com>
2025-07-22 19:54:27 +0800

    work: working vbo/vao

--- src/Main.java
+++ src/Main.java
@@ -20,6 +20,8 @@ public class Main {
         };
 
 
+
+
         if(!glfwInit()){
             throw new IllegalStateException("failed to execute glfw");
         }
@@ -33,52 +35,18 @@ public class Main {
 
         glfwSetWindowPos(window, (vidMode.width() - 640) / 2, (vidMode.height() - 400) / 2 );
 
-        // passed the context from GL to window
+
         glfwMakeContextCurrent(window);
         GL.createCapabilities();
-        texture textureId = new texture("assets/texture.png");
+        Shapes tria = new Shapes(vertices);
 
-        glEnable(GL_TEXTURE_2D);
-        glBindTexture(GL_TEXTURE_2D, textureId.getTextureId());
 
-        long lastTime = System.nanoTime();
-
-        // x and y axis variablesW
-
-        float x = 0;
-        float y = 0;
-
-        float speed = 0.5f;
 
         while(!glfwWindowShouldClose(window)){
-            long currentTime = System.nanoTime();
-            float deltaTime = (currentTime - lastTime) / 1_000_000_000.0f;
-            lastTime = currentTime;
+                glfwPollEvents();
 
-            glfwPollEvents();
-            // controls
-            if(glfwGetKey(window, GLFW_KEY_W) == 1){
-                y += (speed * deltaTime);
-            }if(glfwGetKey(window, GLFW_KEY_S) == 1 ){
-                y -= (speed * deltaTime);
-            }if(glfwGetKey(window, GLFW_KEY_D) == 1){
-                x += (speed * deltaTime);
-            }if(glfwGetKey(window, GLFW_KEY_A) == 1){
-                x -= (speed * deltaTime);
-            }
+                tria.render();
 
-            // clears background
-            glClearColor(0f, 0f, 0f, 1f);
-            glClear(GL_COLOR_BUFFER_BIT);
-
-            // vertices // shapes
-
-            glBegin(GL_QUADS);
-            glTexCoord2f(0f, 0f);    glVertex2f(-0.3f + x, 0.6f + y);
-            glTexCoord2f(0f, 1f);    glVertex2f(-0.3f + x, -0.6f + y);
-            glTexCoord2f(1f, 1f);    glVertex2f(0.3f + x, -0.6f + y);
-            glTexCoord2f(1f, 0f);    glVertex2f(0.3f + x, 0.6f + y);
-            glEnd();
 
             // renders all gl above
             glfwSwapBuffers(window);
